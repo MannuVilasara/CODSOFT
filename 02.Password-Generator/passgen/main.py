6bb1b2ddc0189fa3c7a6e824e9254a91177b5809
MannuVilasara <mannuvilasara@gmail.com>
2024-06-02 16:05:42 +0530

    fix:👾 (02.Password-Generator) Wrong Length of pass

--- 02.Password-Generator/passgen/main.py
+++ 02.Password-Generator/passgen/main.py
@@ -9,7 +9,7 @@ def include_characters() -> str:
     """
     Asks the user if they want to include characters in the password.
     """
-    ask = input(f"{Fore.CYAN}Do You want to inclue special charachters other than the following{Fore.GREEN}(!@#$%^&*- _~+-=): {Style.RESET_ALL}(y/N)")
+    ask = input(f"{Fore.CYAN}Do You want to inclue special charachters other than the following{Fore.GREEN}(!@#$%^&*-_~+-=): {Style.RESET_ALL}(y/N)")
     if ask.lower() == "y":
         characters = input(f"{Fore.CYAN}Enter Characters without space or comma: {Style.RESET_ALL}")
         return characters
@@ -19,7 +19,7 @@ def include_characters() -> str:
 def generate_password(length: int, include_characters: str = "") -> string:
     
     # Define the characters that can be used in the password
-    characters = string.ascii_letters + string.digits + "!@#$%^&*- _~+-=" + include_characters
+    characters = string.ascii_letters + string.digits + "!@#$%^&*-_~+-=" + include_characters
     
     # Generate the password
     password = ''.join(random.choice(characters) for _ in range(length))
@@ -30,10 +30,13 @@ def generate_password(length: int, include_characters: str = "") -> string:
 def get_length() -> int:
     length = input(f"{Fore.CYAN}Number of Digits for the password {Fore.GREEN}(default:16): {Style.RESET_ALL}")
     if length:
-        length = int(length)
-        if length < 8 or length > 32:
-            print(f"{Fore.RED}NOTE: {Fore.LIGHTRED_EX}Password length must be between 8 to 32 characters{Style.RESET_ALL}")
-            get_length()
+        try:
+            length = int(length)
+            if length < 8 or length > 32:
+                print(f"{Fore.RED}NOTE: {Fore.LIGHTRED_EX}Password length must be between 8 to 32 characters{Style.RESET_ALL}")
+                length = get_length()
+        except:
+            length = 16
         return length
     return 16
 
