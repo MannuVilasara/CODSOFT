bae1550ecb501c010aeca01853bf9e5707adcf39
MannuVilasara <mannuvilasara@gmail.com>
2024-06-02 19:35:21 +0530

    fix: 👾

--- 02.Password-Generator/setup.py
+++ 02.Password-Generator/setup.py
@@ -7,7 +7,7 @@ setup(
     author="MannuVilasra",
     author_email="mannuvilasara@gmail.com",
     description="A simple cli-based password generator",
-    version="2.0",
+    version="1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["colorama", "pyperclip"],
